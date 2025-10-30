3749c5ecbaa143da5bfedf657c52e1ddc2d5de06
Edy <edykzaz@gmail.com>
2025-10-30 10:50:15 -0600

    feat: correción TAg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,16 @@
     import { gsap } from 'gsap';
     import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 
+    onMount(() => {
+        // Asegúrate de que esto solo se ejecute en el navegador
+        if (browser) {
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W783R3C7');
+        }
+    });
     // *** Importar datos del servidor ***
     // Esta variable 'data' será poblada por la función 'load' en +page.server.js
     export let data;
